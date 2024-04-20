e2ef7d81918decf27f140ee1649a9e09ac41b703
ADL.LAHIRUD <lahiru.danushka@axiatadigitallabs.com>
2024-04-20 23:47:04 +0530

    initial commit

---
+++ src/main/java/com/example/cicitest/CiCiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicitest/CiCiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
